26421b94fd76602329a1da84b6394148b9dafe10
Dangingxiong <1030227026@qq.com>
2025-03-12 10:49:48 +0800

    original project

---
+++ CCB-BackendServer/src/test/java/com/ecnu/CCB_BackendServer/CcbBackendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecnu.CCB_BackendServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcbBackendServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
