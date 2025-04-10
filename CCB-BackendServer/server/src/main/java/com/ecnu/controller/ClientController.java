b65fb5069991a56fc59b67b63d08e47d3538cce2
Chen Tao <chandagaz84521@gmail.com>
2025-04-10 08:07:57 +0800

    CT：修复了一个存在了很久的bug

--- CCB-BackendServer/server/src/main/java/com/ecnu/controller/ClientController.java
+++ CCB-BackendServer/server/src/main/java/com/ecnu/controller/ClientController.java
@@ -60,7 +60,7 @@ public class ClientController {
 
         ClientSessionVO clientSessionVO = clientService.getSession();
 
-        return Result.success();
+        return Result.success(clientSessionVO);
     }
 
 
