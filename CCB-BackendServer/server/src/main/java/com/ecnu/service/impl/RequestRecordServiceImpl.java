07c36dcbc1d978f5341fdef95154fecf8273b092
WhizzK <1805365107@qq.com>
2025-04-25 21:33:06 +0800

    返回历史聊天记录将senderId更改为senderName

--- CCB-BackendServer/server/src/main/java/com/ecnu/service/impl/RequestRecordServiceImpl.java
+++ CCB-BackendServer/server/src/main/java/com/ecnu/service/impl/RequestRecordServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecnu.service.impl;
 
 import com.ecnu.entity.RequestRecord;
+import com.ecnu.entity.SessionRecord;
+import com.ecnu.entity.User;
 import com.ecnu.mapper.RequestRecordMapper;
+import com.ecnu.mapper.UserMapper;
 import com.ecnu.service.RequestRecordService;
 import com.ecnu.vo.RequestRecordVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class RequestRecordServiceImpl implements RequestRecordService {
@@ -16,16 +20,30 @@ public class RequestRecordServiceImpl implements RequestRecordService {
     @Autowired
     private RequestRecordMapper requestRecordMapper;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public List<RequestRecordVO> getHistoryMessages(Long requestId, Long page, Long size) {
         List<RequestRecord> records =  requestRecordMapper.selectByRequestId(requestId, 0L, 0L);
+
+        if (records == null || records.isEmpty()) {
+            return null;
+        }
+
         List<RequestRecordVO> vos = new ArrayList<RequestRecordVO>();
+
+        RequestRecord msg = records.get(0);
+        User user1 = userMapper.getById(msg.getSenderId());
+        User user2 = userMapper.getById(msg.getReceiverId());
+
         for (RequestRecord record : records) {
+            String senderName = (Objects.equals(record.getSenderId(), user1.getUserId())) ? user1.getRealName() : user2.getRealName();
             RequestRecordVO vo = RequestRecordVO.builder()
                     .requestId(record.getRequestId())
                     .content(record.getContent())
                     .createdAt(record.getCreatedAt())
-                    .senderId(record.getSenderId())
+                    .senderName(senderName)
                     .build();
             vos.add(vo);
         }
